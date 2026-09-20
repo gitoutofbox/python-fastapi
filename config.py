5aff1d31b5ac794875d4c0665a988419980bef77
Sourav <sourav.kr.paul@gmail.com>
2026-09-20 16:34:31 +0530

    fix login

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Settings:
     UPLOAD_DIR = os.getenv("UPLOAD_DIR")
     JWT_SECRET = os.getenv("JWT_SECRET")
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
-    JWT_EXPIRE_MINUTES = os.getenv("JWT_EXPIRE_MINUTES")
+    JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES", "30"))
 
 settings = Settings()
